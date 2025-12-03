89f9c43bf5c6316faca04af46019047fd0b41aaa
HR183 <randikaheshan183@gmail.com>
2025-12-03 20:51:02 +0530

    Add edit profile section

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/profile-forms/CreateProfile";
+import EditProfile from "./components/profile-forms/EditProfile";
 import PrivateRoute from "./components/routing/PrivateRoute";
 // Redux
 import { Provider } from "react-redux";
@@ -50,6 +51,14 @@ const App = () => {
                 element={
                   <PrivateRoute>
                     <CreateProfile />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/edit-profile"
+                element={
+                  <PrivateRoute>
+                    <EditProfile />
                   </PrivateRoute>
                 }
               />
