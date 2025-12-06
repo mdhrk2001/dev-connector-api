e1aa702da37a0130ded2c775254ee6f5c08c29c6
HR183 <randikaheshan183@gmail.com>
2025-12-06 23:41:54 +0530

    Comment Display & Delete

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import PostItem from "../posts/PostItem";
 import CommentForm from "./CommentForm";
+import CommentItem from "./CommentItem";
 import { getPost } from "../../actions/post";
 import { Link, useParams } from "react-router-dom";
 
@@ -22,6 +23,11 @@ const Post = ({ getPost, post: { post, loading } }) => {
       </Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
