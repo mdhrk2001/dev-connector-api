586985b5881bc2bbe01d2e3f5f402f2c103a0f40
HR183 <randikaheshan183@gmail.com>
2025-12-13 18:14:31 +0530

    Add Not Found Page & Theme Workaround

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import Posts from "./components/posts/Posts";
 import Post from "./components/post/Post";
+import NotFound from "./components/layout/NotFound";
 import PrivateRoute from "./components/routing/PrivateRoute";
 
 // Redux
@@ -104,6 +105,7 @@ const App = () => {
                   </PrivateRoute>
                 }
               />
+              <Route path="*" element={<NotFound />} />
             </Routes>
           </section>
         </Fragment>
